87f042baa3f56fd7920290f62c38e15d133a3153
Fernando Birck <fergobirck@gmail.com>
2025-03-23 15:09:54 -0300

    Melhor estruturado para scripting

--- opencutlist_to_cortecloud.py
+++ opencutlist_to_cortecloud.py
@@ -1,19 +1,19 @@
 import csv
 import sys
 
-if len(sys.argv) <= 2:
+def usage():
     print("Utilitário de conversão do OpenCutList do SketchUp para o padrão do CorteCloud")
     print("Uso: python opencutlist_to_cortecloud.py csv_input csv_output")
-    print("Por: Fernando Birck (Fergo) - 2025")
-else:
-    entrada = sys.argv[1]
-    saida = sys.argv[2]
+    print("Por: Fernando Birck (Fergo) - 2025 - github.com/Fergo")
 
+    return 0
+
+def main(entrada, saida):
+    pecas = []
     with open(entrada) as csv_entrada:
         csv_reader = csv.reader(csv_entrada, delimiter=';')
 
         # nem seria necessario todo esse dicionario, mas criei pensando em funcionalidades futuras
-        pecas = []
         for row in csv_reader:
             if row[9] == "Sheet Goods":
                 pecas.append({
@@ -30,7 +30,19 @@ else:
                     'girar': ""
                 })
 
-    with open(saida, 'w') as csv_saida:
-        saida_formatada = []
-        saida_formatada += ["\t".join(str(coluna) for coluna in peca.values()) for peca in pecas]  
-        csv_saida.write("\n".join(saida_formatada))
+    if len(pecas) == 0:
+        print("Nenhuma peça encontrada.")
+        return 1
+    else:
+        with open(saida, 'w') as csv_saida:
+            saida_formatada = []
+            saida_formatada += ["\t".join(str(coluna) for coluna in peca.values()) for peca in pecas]  
+            csv_saida.write("\n".join(saida_formatada))
+        
+        return 0
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 2:
+        usage()
+    else:
+        sys.exit(main(sys.argv[1], sys.argv[2]))
